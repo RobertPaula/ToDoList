816c9822220a3387e64022ad04a358930817e45d
Robert Paula <rpwizard7@gmail.com>
2020-06-04 13:18:39 -0500

    update to the inserted code in the ejs template file: video: 284

--- app.js
+++ app.js
@@ -1,74 +1,69 @@
 // jshint esversion:6
 
-
-// // My modules:
-// const express = require("express");
-// const bodyParser = require("body-parser");
-
-
-
-// // My App:
-//    const app = express();
-
-//    app.get('/', function(req, res) {
-      
-//       var today = new Date();
-//       var currentDay = today.getDay();
-      
-      
-//    });
-
-
-//       // port heroku or local on 4000
-//       app.listen(4000, function(){ // A DYNAMIC PORT that Heroku will define on the go
-//          console.log('This Server is Running on Port 4000 For the ToDo App!');
-//    })
-
+// My modules:
 let express = require('express');
-const bodyParser = require("body-parser");
+let bodyParser = require("body-parser");
 
 // My App:
-let app = express();
+  let app = express(); // initiates express
 
-app.set('view engine', 'ejs');
+   app.set('view engine', 'ejs'); // initiates ejs view engine
 
-app.get('/', (req, res) => {
+   // get route to the home route
+   app.get('/', (req, res) => {
 
-   let today = new Date();
-   let currentDay = today.getDay();
-   let day = "";
+    // logic:
+      let today = new Date();
+      let currentDay = today.getDay();
+      let day = "";
 
-      // response from our server to the browser
-      switch (currentDay) {
-         case 0:
-            day = "Sunday";
-            break;
-         case 1:
-            day = "Monday";
-            break;
-         case 2:
-            day = "Tuesday";
-            break;
-         case 3:
-            day = "Wednesday";
-            break;
-         case 4:
-            day = "Thursday";
-            break;
-         case 5:
-            day = "Friday";
-            break;
-         case 6:
-            day = "Saturday";
-            break;
-         default:
+    // response from our server to the browser
+       switch (currentDay) {
+          case 0:
+             day = "Sunday";
+             break;
+          case 1:
+             day = "Monday";
+             break;
+          case 2:
+             day = "Tuesday";
+             break;
+          case 3:
+             day = "Wednesday";
+             break;
+          case 4:
+             day = "Thursday";
+             break;
+          case 5:
+             day = "Friday";
+             break;
+          case 6:
+             day = "Saturday";
+             break;
+          default:
 
          //throws an error
          console.log("Error: current day is equal to:" + currentDay);
 
-      }// Switch Statement
+       }// Switch Statement
 
-      res.render('list', {DAY : day});
-}); // app.get Route
+             res.render('list', {DAY : day});
 
-app.listen(4000, () => console.log('Example app listening on port 4000!'));
+
+  }); // app.get Route
+
+
+   // res.send(); // this last piece is need to send our whole .get response to the browser
+
+
+
+   // port heroku or local on 4000
+   app.listen(process.env.PORT || 4000, function(){ // A DYNAMIC PORT that Heroku will define on the go
+      console.log('This Server is Running on Port 4000 For the ToDo App!');
+
+   });// app.listen
+
+   // SideNote:
+   // res.send; can only be used once in every .get() method
+   // res.write; can be used to send multiple pieces of data to the browser
+   // res.sendFile(); this sends a html static page to the browser
